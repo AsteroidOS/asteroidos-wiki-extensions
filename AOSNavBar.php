e82f119bb9d53d0e9d81867d9c7cc29afbd46dac
Darrel Griët <dgriet@gmail.com>
2023-06-24 22:57:35 +0200

    Import website styling.
    With minor changes such that it actually works (adjusting paths, fixing CSS errors)
    
    Signed-off-by: Darrel Griët <dgriet@gmail.com>

--- AOSNavBar.php
+++ AOSNavBar.php
@@ -10,24 +10,37 @@ namespace MediaWiki\Extensions\AsteroidOS;
  * @var string $aosNavBarSelectedDefault
  */
 ?>
-<div id="aosnavbar" class="noprint mw-no-invert mw-page-container">
-    <div id="aosnavbarlogo">
-        <p><a id="logo" href="<?= $aosHome ?>"></a></p>
+<header class="navbar navbar-inverse navbar-fixed-top docs-nav" role="banner">
+  <div class="container">
+    <div class="navbar-header">
+      <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a href="<?= $aosHome ?>" class="navbar-brand" alt="AsteroidOS">
+        <img src="data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" height="50" width="50">
+      </a>
     </div>
-    <div id="aosnavbarmenu">
-        <ul id="aosnavbarlist">
-            <?php
-            foreach ($aosNavBar as $name => $url) {
-                if (($title == $name && in_array($name, $aosNavBarSelected))
-                    || (!(in_array($title, $aosNavBarSelected)) && $name == $aosNavBarSelectedDefault)) {
-                    $anbClass = ' class="anb-selected"';
-                } else {
-                    $anbClass = '';
-                }
-                ?>
-            <li id="anb-<?= strtolower($name) ?>"<?= $anbClass ?>><a href="<?= $url ?>"><?= $name ?></a></li><?php
+    <nav class="collapse navbar-collapse bs-navbar-collapse" role="navigation">
+      <ul class="nav navbar-nav navbar-right">
+        <?php
+        foreach ($aosNavBar as $name => $url) {
+            if (($title == $name && in_array($name, $aosNavBarSelected))
+                || (!(in_array($title, $aosNavBarSelected)) && $name == $aosNavBarSelectedDefault)) {
+                $anbClass = ' class="anb-selected"';
+            } else {
+                $anbClass = '';
             }
             ?>
-        </ul>
-    </div>
-</div>
+        <li <?= $anbClass ?>>
+            <a href="<?= $url ?>"><?= $name ?></a>
+        </li>
+        <?php
+        }
+        ?>
+      </ul>
+    </nav>
+  </div>
+</header>
